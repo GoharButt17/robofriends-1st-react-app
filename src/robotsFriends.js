9f9ec493bc1978faec29e3666cd0ba34f8162c32
GoharButt17 <139701860+GoharButt17@users.noreply.github.com>
2023-07-27 15:24:24 +0500

    DEVELOPED FIRST REACT APP

---
+++ src/robotsFriends.js
@@ -0,0 +1,74 @@
+export const robots =[
+    {
+        id : 1,
+        name : 'Gohar Butt',
+        username : 'Gohar',
+        email : 'buttgohar399@gmail.com'
+    },
+    {
+        id : 2,
+        name : 'Sahir Butt',
+        username : 'Sahir',
+        email : 'sahirbutt@gmail.com'
+    },
+    {
+        id : 3,
+        name : 'Ghani Butt',
+        username : 'Ghani',
+        email : 'ghanibutt399@gmail.com'
+    },
+    {
+        id : 4,
+        name : 'Afeera Butt',
+        username : 'Mano',
+        email : 'afeeragohar399@gmail.com'
+    },
+    {
+        id : 5,
+        name : 'Abdul Waheed Butt',
+        username : 'Waheed',
+        email : 'abdulwaheed@gmail.com'
+    },
+    {
+        id : 6,
+        name : 'Wareesha Akeel',
+        username : 'Wareesha',
+        email : 'wareeshamalik@gmail.com'
+    },
+    {
+        id : 7,
+        name : 'Abdul-Rehman Bhatti',
+        username : 'Abdul-Rehman',
+        email : 'bhattiabdul@gmail.com'
+    },
+    {
+        id : 8,
+        name : 'Zain Safdar',
+        username : 'Zain',
+        email : 'zainsafdar@gmail.com'
+    },
+    {
+        id : 9,
+        name : 'Faizan Ahmad',
+        username : 'Faizan',
+        email : 'faizanAhmad@gmail.com'
+    },
+    {
+        id : 10,
+        name : 'Arbaz Rana',
+        username : 'Arbaz',
+        email : 'ranaArbaz@gmail.com'
+    },
+    {
+        id : 11,
+        name : 'Ahmad Nawaz',
+        username : 'Ahmad',
+        email : 'nawazAhmad@gmail.com'
+    },
+    {
+        id : 12,
+        name : 'Mosa Butt',
+        username : 'Mosa',
+        email : 'buttmosa@gmail.com'
+    },
+];
